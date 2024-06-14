0283e5bb5a087e8c7a7512e56fdb30db6cc47dc3
rnktlhr <rinakitluhur10@gmail.com>
2024-06-14 14:12:59 +0700

    menambahkan looping if pada function void inorder

--- binarytree.cpp
+++ binarytree.cpp
@@ -68,6 +68,14 @@ public:
     }
 
     void inorder(Node* ptr){
-        
+        if(ROOT == NULL){
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if(ptr != NULL){
+            inorder(ptr->leftchild);
+            cout << ptr->info << "  ";
+            inorder(ptr->rightchild);
+        }
     }
 };
